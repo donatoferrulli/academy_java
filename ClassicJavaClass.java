bdec20821d39be2dc40ffd865f4389d64f6ff5fc
Giovanni Pontillo <giovanni.pontillo@finconsgroup.com>
2025-03-26 16:40:36 +0100

    Fix java class

--- ClassicJavaClass.java
+++ ClassicJavaClass.java
@@ -1,7 +1,16 @@
 public class ClassicJavaClass {
+
     public class BufferedInputStreamFactoryAdapterDaoInterfaceImplementationServerServiceHandler {
-        public static String execute() {
-            return "lol";
+
+        public static String execute(String input) {
+            System.out.println(input);
+            return "ginopippo";
         }
+
+        public static String getMagia() {
+            return "MAGIAAA";
+        }
+
     }
+    
 }
